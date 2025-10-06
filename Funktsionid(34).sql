1075a306946b9517930859420810a7ab78d5664b
Anastasiia Radasheva <eha20082@gmail.com>
2025-10-06 10:25:56 +0300

    Tabeli Kustutamine(#PersonDetails1) ja protseduuri loomine kohaliku tabeli loomiseks

--- Funktsionid(34).sql
+++ Funktsionid(34).sql
@@ -8,3 +8,22 @@ insert into #PersonDetails1 values (2, 'John')
 insert into #PersonDetails1 values (3, 'Todd')
 select * from #PersonDetails1
 
+
+--tabeli kustutamine
+drop table #PersonDetails1
+
+
+
+--protseduuri loomine kohaliku tabeli loomiseks
+Create procedure spCreateLocalTempTable
+as
+begin
+Create table #PersonDetails1(Id int, Name nvarchar(20))
+insert into #PersonDetails1 values(1, 'Mike')
+insert into #PersonDetails1 values (2, 'John')
+insert into #PersonDetails1 values (3, 'Todd')
+select * from #PersonDetails1
+end
+
+--käivitamine
+exec spCreateLocalTempTable;
