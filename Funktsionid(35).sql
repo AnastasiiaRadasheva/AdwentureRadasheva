f88072d0d1b8997b2c7ee067a263a9e9949805ac
Anastasiia Radasheva <eha20082@gmail.com>
2025-10-06 10:42:47 +0300

    Soovime leida kõik töölised, kelle palk on vahemikus 20000 kuni 70000.(35funk)

---
+++ Funktsionid(35).sql
@@ -0,0 +1,5 @@
+
+select * from DimCustomer;
+
+--Soovime leida kõik töölised, kelle palk on vahemikus 20000 kuni 70000. 
+Select * from DimCustomer where YearlyIncome > 20000 and YearlyIncome < 70000
