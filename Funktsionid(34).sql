192ff44f1adaf16c7b56cf91452434e3799008c3
Anastasiia Radasheva <eha20082@gmail.com>
2025-10-06 10:21:39 +0300

    loome local tabeli PersonDetalis

---
+++ Funktsionid(34).sql
@@ -0,0 +1,10 @@
+
+
+--loome Local tabeli PersonDetails 
+
+create table #PersonDetails1(Id int, Name nvarchar(20))
+insert into #PersonDetails1 values(1, 'Mike')
+insert into #PersonDetails1 values (2, 'John')
+insert into #PersonDetails1 values (3, 'Todd')
+select * from #PersonDetails1
+
