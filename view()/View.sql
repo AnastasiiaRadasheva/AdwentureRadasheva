47b934911273f9429a6cad93182ac8646bab8906
Opilane TTHK <opilane@tthk.ee>
2025-10-09 15:00:07 +0300

    lisa view, vWEmployeesNonConfidentialData mis näed EmployeeKey, firstname, gender, sales territory Region

--- view()/View.sql
+++ view()/View.sql
@@ -1,6 +1,7 @@
 Select * from DimEmployee
 Select * from DimSalesTerritory
 
+--39. View SQL serveris
 
 --lisa view, vWITDepartment_Employees mis näed EmployeeKey, firstname, gender, sales territory Region
 
@@ -10,7 +11,21 @@ select EmployeeKey, FirstName, Gender, SalesTerritoryRegion
 from DimEmployee t
 join DimSalesTerritory tk
 on t.SalesTerritoryKey= tk.SalesTerritoryKey 
-where tk.SalesTerritoryRegion = 'IT' 
+where tk.SalesTerritoryRegion = 'Europe' 
 
 
 select * from vWITDepartment_Employees
+
+
+
+
+--lisa view, vWEmployeesNonConfidentialData mis näed EmployeeKey, firstname, gender, sales territory Region 
+create view vWEmployeesNonConfidentialData
+as
+Select EmployeeKey, FirstName,
+Gender,SalesTerritoryRegion
+from DimEmployee t
+join DimSalesTerritory tk
+on t.SalesTerritoryKey= tk.SalesTerritoryKey 
+--käivitama view
+select * from vWEmployeesNonConfidentialData
